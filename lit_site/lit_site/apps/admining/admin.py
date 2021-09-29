2832186f0cf3acfe161e5cd1c1fbbcda0bee52d5
Valentine Shimansky <55807331+MrRaisRay@users.noreply.github.com>
2021-09-29 23:55:19 +0300

    Make editing function for admin

--- lit_site/lit_site/apps/admining/admin.py
+++ lit_site/lit_site/apps/admining/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import About, Vacancy, Picture, Appeal
 # Register your models here.
+
+admin.site.register(About)
+admin.site.register(Vacancy)
+admin.site.register(Picture)
+admin.site.register(Appeal)
