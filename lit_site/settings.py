b9063c58e2783976cbebf708fd65bdc024770a02
Valentine Shimansky <55807331+MrRaisRay@users.noreply.github.com>
2021-10-04 10:10:50 +0300

    Diploy

--- lit_site/settings.py
+++ lit_site/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,6 +130,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'lit_site/static')
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR, "lit_site/static"),
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
